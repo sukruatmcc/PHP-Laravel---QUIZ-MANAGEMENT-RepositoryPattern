9f7e9817bfb9bdd598d796fe0feb04674337de36
sukruatmcc <github5838@gmail.com>
2023-12-26 23:59:20 +0300

    Initial commit

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    public function bindRepositories()
+    {
+
+    }
+}
